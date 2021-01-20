9ecd43ba6b3b404d06d13e40b90dc937c3ae2649
Ridwan Taiwo <donriddo@gmail.com>
2021-01-20 15:29:49 +0100

    clean up codebase

--- config/db.js
+++ config/db.js
@@ -1,36 +1,36 @@
-const mongoose = require( 'mongoose' );
-mongoose.set( 'useFindAndModify' , false );
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 
 const options = {
-  keepAlive : true ,
-  connectTimeoutMS : 30000 ,
-  useNewUrlParser : true ,
-  useUnifiedTopology : true
+  keepAlive: true,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 };
 
 class db {
-  connect( DB_URL ) {
+  connect(DB_URL) {
     const log = console;
-    mongoose.connect( DB_URL , options )
-      .then( async () => {
-        log.info( `Successfully connected to ${ DB_URL }` );
-      } )
-      .catch( ( err ) => {
-        log.error( `There was a db connection error ${ err }` );
-        process.exit( 0 );
-      } );
-    mongoose.set( 'useCreateIndex' , true );
+    mongoose.connect(DB_URL, options)
+      .then(async () => {
+        log.info(`Successfully connected to ${DB_URL}`);
+      })
+      .catch((err) => {
+        log.error(`There was a db connection error ${err}`);
+        process.exit(0);
+      });
+    mongoose.set('useCreateIndex', true);
     const db = mongoose.connection;
 
-    db.once( 'disconnected' , () => {
-      log.error( `Successfully disconnected from ${ DB_URL }` );
-    } );
-    process.on( 'SIGINT' , () => {
-      mongoose.connection.close( () => {
-        log.error( 'dBase connection closed due to app termination' );
-        process.exit( 0 );
-      } );
-    } );
+    db.once('disconnected', () => {
+      log.error(`Successfully disconnected from ${DB_URL}`);
+    });
+    process.on('SIGINT', () => {
+      mongoose.connection.close(() => {
+        log.error('dBase connection closed due to app termination');
+        process.exit(0);
+      });
+    });
   }
 }
 
