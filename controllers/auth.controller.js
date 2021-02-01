cbad4031e9b3adba22aa6bde5e389a645c315dd0
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2021-02-01 17:45:58 +0100

    more fixes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,12 +2,12 @@ const { pick } = require('lodash');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const createError = require('http-errors');
-const { customAlphabet } = require("nanoid")
+const { customAlphabet } = require('nanoid');
 const { validate } = require('../utils/validator');
-const User = require("../models/user.model")
-const Otp = require("../models/otp.model")
-const { sendMail } = require("../services/mailService")
-const moment = require("moment")
+const User = require('../models/user.model');
+const Otp = require('../models/otp.model');
+const { sendMail } = require('../services/mailService');
+const moment = require('moment');
 class AuthController {
   static async login(req, res, next) {
     try {
@@ -19,12 +19,15 @@ class AuthController {
         if (!user) return res.status(401).json(info);
 
         let token = jwt.sign(
-          pick(
-            user.toJSON(),
-            ['_id', 'type', 'role', 'email', 'isEmailVerified']
-          ),
+          pick(user.toJSON(), [
+            '_id',
+            'type',
+            'role',
+            'email',
+            'isEmailVerified',
+          ]),
           process.env.JWT_SECRET,
-          { expiresIn: process.env.TOKEN_EXPIRY || 604800 }, // 7 days
+          { expiry: process.env.TOKEN_EXPIRY || 604800 }, // 7 days
         );
 
         jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
@@ -33,16 +36,15 @@ class AuthController {
             data: {
               user,
               token: token,
-              expiry: decoded.exp
-            }
+              expiry: decoded.exp,
+            },
           });
         });
-
       })(req, res);
     } catch (error) {
       next(error);
     }
-  };
+  }
 
   static async changePassword(req, res, next) {
     try {
@@ -50,144 +52,164 @@ class AuthController {
 
       const user = req.user;
 
-      const isOldPasswordCorrect = await user.validPassword(req.body.oldPassword);
-
+      const isOldPasswordCorrect = await user.validPassword(
+        req.body.oldPassword,
+      );
 
       if (!isOldPasswordCorrect) {
-        throw createError(400, 'old password incorrect')
+        throw createError(400, 'old password incorrect');
       }
 
       user.password = req.body.newPassword;
       await user.save();
 
       return res.status(200).json({
-        message: 'password changed successfully'
+        message: 'password changed successfully',
       });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  };
+  }
   static async requestPasswordReset(req, res, next) {
     try {
       let { email } = req.body;
-      AuthController.validateRequest(req.body, "reset-password")
+      AuthController.validateRequest(req.body, 'reset-password');
       let user = await User.findOne({ email });
       if (!user) {
         return res.status(403).json({
-          message: "No account found with this email"
-        })
+          message: 'No account found with this email',
+        });
       } else {
         // expire previous Otps
         await Otp.updateMany(
-          { user: user.id, isExpired: false, type: "reset-password" },
+          {
+            user: user.id,
+            isExpired: false,
+            type: 'reset-password',
+          },
           { isExpired: true },
         );
-        let expiry = moment.utc().add(1, 'hours')
+        let expiry = moment.utc().add(1, 'hours');
         let otp = await Otp.create({
-          otp: customAlphabet("23456789ADFGHJKLMNBVCXZPUYTREWQ", 8)(),
-          type: "reset-password",
+          otp: customAlphabet('23456789ADFGHJKLMNBVCXZPUYTREWQ', 8)(),
+          type: 'reset-password',
           user,
           expiry,
         });
-        let message = `Use this code to reset your password: ${otp.otp}. This code expires in 1 hour`
-        await sendMail("Password Reset", user.email, message)
+        let message = `Use this code to reset your password: ${otp.otp}. This code expires in 1 hour`;
+        await sendMail('Password Reset', user.email, message);
         return res
           .status(200)
-          .json({ message: "A reset code has been sent to your email address" })
+          .json({
+            message: 'A reset code has been sent to your email address',
+          });
       }
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
   static async resetPassword(req, res, next) {
     try {
       AuthController.validateRequest(req.body, 'set-password');
-      let otp = await Otp.findOne({ otp: req.params.otp }).populate("user");
+      let otp = await Otp.findOne({ otp: req.params.otp }).populate('user');
       if (!otp) {
-        return res.status(401).json({ message: "Invalid Recovery Code" })
+        return res.status(401).json({ message: 'Invalid Recovery Code' });
       }
-      if (otp.isExpired || moment.utc(otp.expiresIn).diff(moment.utc(), "hours") > 0) {
+      if (
+        otp.isExpired ||
+        moment.utc(otp.expiry).diff(moment.utc(), 'hours') > 0
+      ) {
         // expire the otp if it is just a date difference
         otp.isExpired = true;
-        await otp.save()
-        return res.status(401).json({ message: "This code is expired. Please request a new one" })
+        await otp.save();
+        return res
+          .status(401)
+          .json({ message: 'This code is expired. Please request a new one' });
       }
       let { user } = otp;
       user.password = req.body.password;
       otp.isExpired = true;
-      await otp.save()
+      await otp.save();
       await user.save();
       return res.status(200).json({
-        message: 'Password reset successful'
+        message: 'Password reset successful',
       });
-
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
   static async resendVerificationToken(req, res, next) {
     try {
       let { email } = req.body;
-      AuthController.validateRequest(req.body, "verification")
+      AuthController.validateRequest(req.body, 'verification');
       let user = await User.findOne({ email });
       if (!user) {
         return res.status(403).json({
-          message: "No account found with this email"
-        })
-      };
+          message: 'No account found with this email',
+        });
+      }
       if (user.isEmailVerifed) {
-        return res.status(409).json({ message: "User account already verified" })
+        return res
+          .status(409)
+          .json({ message: 'User account already verified' });
       }
       // expire all previous tokens
 
       await Otp.updateMany(
-        { user: user.id, isExpired: false, type: "verify-email" },
+        { user: user.id, isExpired: false, type: 'verify-email' },
         { isExpired: true },
       );
-      let expiry = moment.utc().add(1, 'hours')
+      let expiry = moment.utc().add(1, 'hours');
       let otp = await Otp.create({
-        otp: customAlphabet("23456789ADFGHJKLMNBVCXZPUYTREWQ", 8)(),
-        type: "verify-email",
+        otp: customAlphabet('23456789ADFGHJKLMNBVCXZPUYTREWQ', 8)(),
+        type: 'verify-email',
         user,
         expiry,
       });
-      console.log({ otp })
+      console.log({ otp });
 
-      let message = `Use this code to verify your email ${otp.otp}. This code expires in 1 hour`
-      await sendMail("Verify you email", user.email, message)
-      return res.status(200).json({ message: "A reset code has been sent to your email address" })
-
+      let message = `Use this code to verify your email ${otp.otp}. This code expires in 1 hour`;
+      await sendMail('Verify you email', user.email, message);
+      return res
+        .status(200)
+        .json({ message: 'A reset code has been sent to your email address' });
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
   static async verifyEmail(req, res, next) {
     try {
-      let otp = await Otp.findOne({ otp: req.params.otp }).populate("user");
+      let otp = await Otp.findOne({ otp: req.params.otp }).populate('user');
       if (!otp) {
-        return res.status(401).json({ message: "Invalid Verification Code" })
+        return res.status(401).json({ message: 'Invalid Verification Code' });
       }
-      if (otp.isExpired || moment.utc(otp.expiresIn).diff(moment.utc(), "hours") > 0) {
+      if (
+        otp.isExpired ||
+        moment.utc(otp.expiry).diff(moment.utc(), 'hours') > 0
+      ) {
         // expire the otp if it is just a date difference
         otp.isExpired = true;
-        await otp.save()
-        return res.status(401).json({ message: "This code is expired. Please request a new one" })
+        await otp.save();
+        return res
+          .status(401)
+          .json({ message: 'This code is expired. Please request a new one' });
       }
       let { user } = otp;
       if (user.isEmailVerifed) {
-        return res.status(409).json({ message: "User account already verified" })
+        return res
+          .status(409)
+          .json({ message: 'User account already verified' });
       }
       user.isEmailVerifed = true;
-      console.log({ otp, user })
+      console.log({ otp, user });
       otp.isExpired = true;
-      await otp.save()
+      await otp.save();
       await user.save();
       return res.status(200).json({
-        message: 'Email verified successfully'
+        message: 'Email verified successfully',
       });
-
     } catch (err) {
-      next(err)
+      next(err);
     }
   }
   static validateRequest(body, action = 'login') {
@@ -217,7 +239,7 @@ class AuthController {
             type: 'string',
             required: true,
           },
-        }
+        };
         break;
 
       case 'set-password':
@@ -226,30 +248,28 @@ class AuthController {
             type: 'string',
             required: true,
           },
-        }
+        };
         break;
       case 'reset-password':
         fields = {
           email: {
             type: 'string',
-            required: true
-          }
-        }
+            required: true,
+          },
+        };
       case 'verification':
         fields = {
           email: {
             type: 'string',
-            required: true
-          }
-        }
+            required: true,
+          },
+        };
       default:
         break;
     }
 
     validate(body, { properties: fields });
   }
-
 }
 
 module.exports = AuthController;
-
